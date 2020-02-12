d2d578dee30400b774a0ca4503e262286564e78b
Bruno Rossini <brunorossini@live.com>
2020-02-12 09:02:08 -0300

    Utilizando component class

---
+++ src/styles/global.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  html, body, #root {
+    min-height: 100%;
+  }
+
+  body {
+    background: #7159c1;
+    -webkit-font-smoothing: antialiased !important;
+  }
+
+  border-style, input, button {
+    color: #222;
+    font-size: 14px;
+  }
+
+  button: {
+    cursor: pointer;
+  }
+`;
